9b56fb8a4ffcad70cdff50a71aa510929e42ff8a
Caitlyn <caitlyn@craigs-iMac.attlocal.net>
2020-08-18 07:58:31 -0700

    finished video 187

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,8 +1,61 @@
 import 'package:flutter/material.dart';
 
+import '../models/product.dart';
+import '../widgets/product_item.dart';
+
 class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = [
+    Product(
+      id: 'p1',
+      title: 'Resin Keychain',
+      description: 'A kpop keychain - pretty lavender color!',
+      price: 4.00,
+      imageUrl:
+          'https://i.etsystatic.com/22983059/r/il/ea8361/2434648058/il_794xN.2434648058_4tm0.jpg',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    ),
+    Product(
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
+    Product(
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        itemCount: loadedProducts.length,
+        itemBuilder: (context, index) => ProductItem(loadedProducts[index].id,
+            loadedProducts[index].title, loadedProducts[index].imageUrl),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+      ),
+    );
   }
 }
