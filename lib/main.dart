a1f7b23b6e55ae0e2e06d7de6385a1d65f61752b
Caitlyn <caitlyn@craigs-iMac.attlocal.net>
2020-08-31 07:45:34 -0700

    finished video 208 Orders Screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import 'package:provider/provider.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => Cart(),
         ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
+        )
       ],
       child: MaterialApp(
         title: 'MyShop',
