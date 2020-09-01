2887b3241107622f6f28d7318870f31e2207de33
Caitlyn <caitlyn@craigs-iMac.attlocal.net>
2020-09-01 07:45:27 -0700

    video 210 6:36

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -5,6 +5,7 @@ import '../widgets/badge.dart';
 import '../providers/cart.dart';
 import 'package:provider/provider.dart';
 import './cart_screen.dart';
+import '../widgets/app_drawer.dart';
 
 enum FilterOptions { Favorites, All }
 
@@ -58,6 +59,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +23,28 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle:
-                Text(DateFormat('dd MM yyyy hh:mm').format(order.dateTime)),
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              height: min(widget.order.products.length * 20.0 + 100, 180),
+              child: ListView(
+                  children: widget.order.products
+                      .map((prod) => Row(
+                            children: [Text(prod.title)],
+                          ))
+                      .toList()),
+            )
         ],
       ),
     );
