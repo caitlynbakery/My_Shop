680e95f5a104b044097edfc891969e0de0991a95
Caitlyn <caitlyn@craigs-iMac.attlocal.net>
2020-09-15 07:45:37 -0700

    video 228 12:53

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../providers/product.dart';
+import '../providers/products.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -17,11 +19,39 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _form = GlobalKey<FormState>();
   var _editedProduct =
       Product(id: null, title: '', price: 0, description: '', imageUrl: '');
+  var _initValues = {
+    'title': '',
+    'description': '',
+    'price': '',
+    'imageUrl': '',
+  };
+  var _isInit = true;
 
   @override
   void initState() {
     _imageUrlFocusNode.addListener(_updateImageUrl);
     super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      final productId = ModalRoute.of(context).settings.arguments as String;
+      if (productId != null) {
+        _editedProduct =
+            Provider.of<Products>(context, listen: false).findById(productId);
+        _initValues = {
+          'title': _editedProduct.title,
+          'description': _editedProduct.description,
+          'price': _editedProduct.price.toString(),
+          //  'imageUrl': _editedProduct.imageUrl,
+          'imageUrl': '',
+        };
+        _imageUrlController.text = _editedProduct.imageUrl;
+      }
+    }
+    _isInit = false;
+    super.didChangeDependencies();
   }
 
   @override
@@ -40,6 +70,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
               !_imageUrlController.text.startsWith('https')) ||
           (!_imageUrlController.text.endsWith('.png') &&
               !_imageUrlController.text.endsWith('.jpg') &&
+              !_imageUrlController.text.endsWith('.webp') &&
               !_imageUrlController.text.endsWith('.jpeg'))) {
         return;
       }
@@ -53,10 +84,13 @@ class _EditProductScreenState extends State<EditProductScreen> {
       return;
     }
     _form.currentState.save();
-    print(_editedProduct.title);
-    print(_editedProduct.description);
-    print(_editedProduct.price);
-    print(_editedProduct.imageUrl);
+    if (_editedProduct.id != null) {
+    } else {
+      Provider.of<Products>(context, listen: false)
+          .addProduct((_editedProduct));
+    }
+
+    Navigator.of(context).pop();
   }
 
   @override
@@ -79,6 +113,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
             child: Column(
               children: [
                 TextFormField(
+                    initialValue: _initValues['title'],
                     decoration: InputDecoration(labelText: 'Title'),
                     textInputAction: TextInputAction.next,
                     onFieldSubmitted: (_) {
@@ -99,6 +134,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
                           id: null);
                     }),
                 TextFormField(
+                    initialValue: _initValues['price'],
                     decoration: InputDecoration(labelText: 'Price'),
                     textInputAction: TextInputAction.next,
                     keyboardType: TextInputType.number,
@@ -128,6 +164,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
                           id: null);
                     }),
                 TextFormField(
+                    initialValue: _initValues['description'],
                     decoration: InputDecoration(labelText: 'Description'),
                     maxLines: 3,
                     keyboardType: TextInputType.multiline,
@@ -188,6 +225,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
                             }
                             if (!value.endsWith('.png') &&
                                 !value.endsWith('.jpg') &&
+                                !value.endsWith('.webp') &&
                                 !value.endsWith('.jpeg')) {
                               return 'Please enter valid image URL';
                             }
