5697a6f7b8dfc9a942b2c71e5c60251efbeecde9
Caitlyn <craigoda+caitlyn@gmail.com>
2020-10-30 07:45:36 -0700

    video 261 9:36

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,10 @@ import 'package:provider/provider.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
+import 'providers/auth.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (ctx) => Auth(),),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
@@ -33,7 +36,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.cyan,
             accentColor: Colors.pink[300],
             fontFamily: 'Lato'),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
