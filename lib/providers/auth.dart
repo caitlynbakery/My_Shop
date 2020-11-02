9d0df3fe342faf04ec7c87d972c8e482a5321be3
Caitlyn <craigoda+caitlyn@gmail.com>
2020-11-02 07:45:31 -0800

    262 Allowing Users to Log in 2:31

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -9,11 +9,24 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
+  Future<void> authenticate(String email, String password, String urlSegment) async {
+
+  }
+
   Future<void> signup(String email, String password) async {
     const url =
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB28k_LfSGGtnK0i74MvASAMaQ8xxW7fxI';
 
     final response = await http.post(url,
+        body: json.encode(
+            {'email': email, 'password': password, 'returnSecureToken': true}));
+            print(json.decode(response.body));
+  }
+
+  Future<void> signIn(String email, String password) async {
+    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB28k_LfSGGtnK0i74MvASAMaQ8xxW7fxI';
+
+      final response = await http.post(url,
         body: json.encode(
             {'email': email, 'password': password, 'returnSecureToken': true}));
             print(json.decode(response.body));
